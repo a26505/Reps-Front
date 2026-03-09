087a57748591b7a699f726a3cb32252701b72068
a26505 <158261466+a26505@users.noreply.github.com>
2026-03-09 02:52:28 +0100

    actualizo conesion base dato

--- vite.config.ts
+++ vite.config.ts
@@ -3,31 +3,26 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 
-//https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-        vuetify({ autoImport: true }),
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    },
-    server: {
-        port: 5173,
-        host: true,
-        allowedHosts: ['.amazonaws.com'], 
-        proxy: {
-            '/api': {
-<<<<<<< HEAD
-                target: 'http://10.100.194.25:80', 
-=======
-               target: 'http://reps-backend-service:5000',
->>>>>>> main-new
-                changeOrigin: true,
-                secure: false,
-            }
-        }
+  plugins: [
+    vue(),
+    vuetify({ autoImport: true }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    port: 5173,
+    host: true,
+    allowedHosts: ['.amazonaws.com'],
+    proxy: {
+      '/api': {
+        target: 'http://reps-backend-service:5000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
